3d42b24d0494fbd2acc6332f54534f82f07eca46
rivaldo dsilva <rivaldodsilva8@gmail.com>
2024-05-25 02:26:08 -0400

    changed stuff to Fox Tow

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -13,9 +13,9 @@ export const Home = () => {
         <div className="hero-overlay bg-opacity-70"></div>
         <div className="hero-content text-center text-neutral-content">
           <div className="max-w-md white">
-            <h1 className="mb-5 text-5xl font-bold">TwoPro</h1>
+            <h1 className="mb-5 text-5xl font-bold">Fox Tow</h1>
             <p className="mb-5 white">
-              Welcome to TowPro, where cutting-edge technology meets
+              Welcome to Fox Tow, where cutting-edge technology meets
               unparalleled efficiency in every tow. Our advanced GPS systems
               ensure swift response times and seamless operations, allowing us
               to reach you faster and provide the assistance you need, when you
